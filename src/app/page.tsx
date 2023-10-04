9133ffb599576587c04ef4a66691c2c1ce760c9d
rerelurelu <awaawapurin@gmail.com>
2023-10-05 02:17:31 +0900

    Revert "👻"
    This reverts commit 38d3f018159bc8633cd6b02871303ea54a39e198.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link'
 
-import { Flex, Input, useBoolean } from '@chakra-ui/react'
+import { Accordion, Button, Flex, Text, useBoolean } from '@chakra-ui/react'
 
 export default function Home() {
   const [isOpen, setIsOpen] = useBoolean(false);
@@ -11,13 +11,14 @@ export default function Home() {
 
   return (
     <>
+      <h1>Hello</h1>
       <Flex mt={20} gap={10}>
         <Link href={'/post/13'}>Post 13</Link>
         <Link href={'/post/33'}>Post 33</Link>
         <Link href={'/post/43'}>Post 43</Link>
         <Link href={'/post/53'}>Post 53</Link>
       </Flex>
-      <Input onChange={setIsOpen.toggle} />
+      <Accordion onChange={setIsOpen.toggle}>ボタン</Accordion>
     </>
   );
 }
